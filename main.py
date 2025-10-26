1422c5a624f3a6c6ff39ca477527f4378e90bdbd
Robin <5983417+rorro@users.noreply.github.com>
2025-10-26 22:47:24 +0100

    Replace scraping with rankings.json endpoint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import abc
 import asyncio
 import logging
 import secrets
@@ -10,7 +9,6 @@ from logging.handlers import RotatingFileHandler
 
 import wom
 from aiohttp import ClientSession
-from bs4 import BeautifulSoup, Tag
 
 #########################################################
 # START Configuration
@@ -23,6 +21,12 @@ METRIC_LIMIT: t.Final[t.Optional[int]] = 3
 """The maximum number of metrics to fetch (set to `None` to fetch all).
 
 This can ease testing without waiting for all metrics to be fetched.
+"""
+
+PLAYERS_PER_PAGE: t.Final[int] = 50
+"""The amount of players to fetch per request.
+
+Maximum is 50.
 """
 
 ENABLE_SEASONAL: t.Final[bool] = False
@@ -119,79 +123,33 @@ class Metric:
         return self._table
 
     @property
-    def category(self) -> t.Optional[int]:
+    def category(self) -> int:
         """The category of the metric, it it has one.
 
         All skills have no category, and everything else is category 1.
         """
         return self._category
-
-
-class MetricLeader(abc.ABC):
-    def __init__(self, metric: Metric, username: str, rank: int) -> None:
-        self._username = username
-        self._metric = metric
+    
+class HiscorePlayer:
+    def __init__(self, name: str, score: int, rank: int) -> None:
+        self._name = name
+        self._score = score
         self._rank = rank
 
-    @property
-    def metric(self) -> Metric:
-        """The metric this player leads in."""
-        return self._metric
-
-    @property
-    def username(self) -> str:
-        """The username of the player leading."""
-        return self._username
-
-    @property
-    def rank(self) -> int:
-        """The players rank in this metric."""
-        return self._rank
-
-    @abc.abstractmethod
     def __str__(self) -> str:
-        """Returns a pretty string representing this metric leader."""
-        ...
-
-
-class SkillLeader(MetricLeader):
-    def __init__(
-        self, metric: Metric, username: str, rank: int, level: int, exp: int
-    ) -> None:
-        super().__init__(metric, username, rank)
-        self._level = level
-        self._exp = exp
+        return self._name
 
     @property
-    def level(self) -> int:
-        """The players level in this skill."""
-        return self._level
-
-    @property
-    def exp(self) -> int:
-        """The players experience in this skill."""
-        return self._exp
-
-    def __str__(self) -> str:
-        return (
-            f"{self.metric.name}: Rank {self.rank} -> {self.username} "
-            f"at level {self.level} with {self.exp} xp"
-        )
-
-
-class NonSkillLeader(MetricLeader):
-    def __init__(self, metric: Metric, username: str, rank: int, score: int) -> None:
-        super().__init__(metric, username, rank)
-        self._score = score
-
+    def name(self) -> str:
+        return self._name
+    
     @property
     def score(self) -> int:
-        """The players (score/kills/points etc) in this metric."""
         return self._score
-
-    def __str__(self) -> str:
-        return f"{self.metric.name}: Rank {self.rank} -> {self.username} with score {self.score}"
-
+    
+    @property
+    def rank(self) -> int:
+        return self._rank
 
 class Group:
     def __init__(self, client: wom.Client, details: wom.GroupDetail) -> None:
@@ -201,7 +159,7 @@ class Group:
     @property
     def members(self) -> t.List[wom.GroupMembership]:
         """The members of this group."""
-        return self._details.memberships
+        return self._details.group.memberships
 
     @property
     def name(self) -> str:
@@ -211,7 +169,7 @@ class Group:
     @property
     def count(self) -> int:
         """The amount of leaders in the group."""
-        return len(self._details.memberships)
+        return len(self._details.group.memberships)
 
     @property
     def id(self) -> int:
@@ -227,11 +185,11 @@ class Group:
         return f"WOM Group {self.name} (id: {self.id}) with {self.count} members"
 
     @classmethod
-    async def create(cls, client: wom.Client, members: t.List[MetricLeader]) -> "Group":
+    async def create(cls, client: wom.Client, members: t.List[HiscorePlayer]) -> "Group":
         """Creates the group on WOM."""
         LOGGER.info("Creating group")
         result = await client.groups.create_group(
-            LEADER_GROUP_NAME, *(wom.GroupMemberFragment(m.username) for m in members)
+            LEADER_GROUP_NAME, *(wom.GroupMemberFragment(m.name) for m in members)
         )
 
         if result.is_err:
@@ -292,112 +250,123 @@ class Group:
 #########################################################
 
 METRICS: t.Final[t.List[Metric]] = [
-    Metric("Overall", 0),
-    Metric("Attack", 1),
-    Metric("Defence", 2),
-    Metric("Strength", 3),
-    Metric("Hitpoints", 4),
-    Metric("Ranged", 5),
-    Metric("Prayer", 6),
-    Metric("Magic", 7),
-    Metric("Cooking", 8),
-    Metric("Woodcutting", 9),
-    Metric("Fletching", 10),
-    Metric("Fishing", 11),
-    Metric("Firemaking", 12),
-    Metric("Crafting", 13),
-    Metric("Smithing", 14),
-    Metric("Mining", 15),
-    Metric("Herblore", 16),
-    Metric("Agility", 17),
-    Metric("Thieving", 18),
-    Metric("Slayer", 19),
-    Metric("Farming", 20),
-    Metric("Runecrafting", 21),  # This is for you Ruben
-    Metric("Hunter", 22),
-    Metric("Construction", 23),
-    Metric("Clue Scolls (all)", 6, 1),
-    Metric("Clue Scolls (beginner)", 7, 1),
-    Metric("Clue Scolls (easy)", 8, 1),
-    Metric("Clue Scolls (medium)", 9, 1),
-    Metric("Clue Scolls (hard)", 10, 1),
-    Metric("Clue Scolls (elite)", 11, 1),
-    Metric("Clue Scolls (master)", 12, 1),
-    Metric("LMS - Rank", 13, 1),
-    Metric("PVP Arena - Rank", 14, 1),
-    Metric("Soul Wars Zeal", 15, 1),
-    Metric("Rifts Closed", 16, 1),
-    Metric("Abyssal Sire", 17, 1),
-    Metric("Alchemical Hydra", 18, 1),
-    Metric("Artio", 19, 1),
-    Metric("Barrows Chests", 20, 1),
-    Metric("Bryophyta", 21, 1),
-    Metric("Callisto", 22, 1),
-    Metric("Calvar'ion", 23, 1),
-    Metric("Cerberus", 24, 1),
-    Metric("Chambers of Xeric", 25, 1),
-    Metric("Chambers of Xeric: Challenge Mode", 26, 1),
-    Metric("Chaos Elemental", 27, 1),
-    Metric("Chaos Fanatic", 28, 1),
-    Metric("Commander Zilyana", 29, 1),
-    Metric("Corporeal Beast", 30, 1),
-    Metric("Crazy Archaeologist", 31, 1),
-    Metric("Dagganoth Prime", 32, 1),
-    Metric("Dagganoth Rex", 33, 1),
-    Metric("Dagganoth Supreme", 34, 1),
-    Metric("Deranged Archaeologist", 35, 1),
-    Metric("Duke Sucellus", 36, 1),
-    Metric("General Graardor", 37, 1),
-    Metric("Giant Mole", 38, 1),
-    Metric("Grotesque Guardians", 39, 1),
-    Metric("Hespori", 40, 1),
-    Metric("Kalphite Queen", 41, 1),
-    Metric("King Black Dragon", 42, 1),
-    Metric("Kraken", 43, 1),
-    Metric("Kree'arra", 44, 1),
-    Metric("K'ril Tsutsaroth", 45, 1),
-    Metric("Mimic", 46, 1),
-    Metric("Nex", 47, 1),
-    Metric("Nightmare", 48, 1),
-    Metric("Phosani's Nightmare", 49, 1),
-    Metric("Obor", 50, 1),
-    Metric("Phantom Muspah", 51, 1),
-    Metric("Sarachnis", 52, 1),
-    Metric("Scorpia", 53, 1),
-    Metric("Skotizo", 54, 1),
-    Metric("Spindel", 55, 1),
-    Metric("Tempoross", 56, 1),
-    Metric("The Gauntlet", 57, 1),
-    Metric("The Corrupted Gauntlet", 58, 1),
-    Metric("The Leviathan", 59, 1),
-    Metric("The Whisperer", 60, 1),
-    Metric("Theatre of Blood", 61, 1),
-    Metric("Theatre of Blood: Hard Mode", 62, 1),
-    Metric("Themonuclear Smoke Devil", 63, 1),
-    Metric("Tombs of Amascut", 64, 1),
-    Metric("Tombs of Amascut: Expert Mode", 65, 1),
-    Metric("TzKal-Zuk", 66, 1),
-    Metric("TzTok-Jad", 67, 1),
-    Metric("Vardorvis", 68, 1),
-    Metric("Venenatis", 69, 1),
-    Metric("Vet'ion", 70, 1),
-    Metric("Vorkath", 71, 1),
-    Metric("Wintertodt", 72, 1),
-    Metric("Zalcano", 73, 1),
-    Metric("Zulrah", 74, 1),
+    Metric("Overall", 0, 0),
+    Metric("Attack", 1, 0),
+    Metric("Defence", 2, 0),
+    Metric("Strength", 3, 0),
+    Metric("Hitpoints", 4, 0),
+    Metric("Ranged", 5, 0),
+    Metric("Prayer", 6, 0),
+    Metric("Magic", 7, 0),
+    Metric("Cooking", 8, 0),
+    Metric("Woodcutting", 9, 0),
+    Metric("Fletching", 10, 0),
+    Metric("Fishing", 11, 0),
+    Metric("Firemaking", 12, 0),
+    Metric("Crafting", 13, 0),
+    Metric("Smithing", 14, 0),
+    Metric("Mining", 15, 0),
+    Metric("Herblore", 16, 0),
+    Metric("Agility", 17, 0),
+    Metric("Thieving", 18, 0),
+    Metric("Slayer", 19, 0),
+    Metric("Farming", 20, 0),
+    Metric("Runecrafting", 21, 0),  # This is for you Ruben
+    Metric("Hunter", 22, 0),
+    Metric("Construction", 23, 0),
+    Metric("Clue Scolls (all)", 7, 1),
+    Metric("Clue Scolls (beginner)", 8, 1),
+    Metric("Clue Scolls (easy)", 9, 1),
+    Metric("Clue Scolls (medium)", 10, 1),
+    Metric("Clue Scolls (hard)", 11, 1),
+    Metric("Clue Scolls (elite)", 12, 1),
+    Metric("Clue Scolls (master)", 13, 1),
+    Metric("LMS - Rank", 14, 1),
+    Metric("PVP Arena - Rank", 15, 1),
+    Metric("Soul Wars Zeal", 16, 1),
+    Metric("Rifts Closed", 17, 1),
+    Metric("Colosseum Glory", 18, 1),
+    Metric("Collections Logged", 19, 1),
+    Metric("Abyssal Sire", 20, 1),
+    Metric("Alchemical Hydra", 21, 1),
+    Metric("Amoxliatl", 22, 1),
+    Metric("Araxxor", 23, 1),
+    Metric("Artio", 24, 1),
+    Metric("Barrows Chests", 25, 1),
+    Metric("Bryophyta", 26, 1),
+    Metric("Callisto", 27, 1),
+    Metric("Calvar'ion", 28, 1),
+    Metric("Cerberus", 29, 1),
+    Metric("Chambers of Xeric", 30, 1),
+    Metric("Chambers of Xeric: Challenge Mode", 31, 1),
+    Metric("Chaos Elemental", 32, 1),
+    Metric("Chaos Fanatic", 33, 1),
+    Metric("Commander Zilyana", 34, 1),
+    Metric("Corporeal Beast", 35, 1),
+    Metric("Crazy Archaeologist", 36, 1),
+    Metric("Dagganoth Prime", 37, 1),
+    Metric("Dagganoth Rex", 38, 1),
+    Metric("Dagganoth Supreme", 39, 1),
+    Metric("Deranged Archaeologist", 40, 1),
+    Metric("Doom of Mokhaiotl", 41, 1),
+    Metric("Duke Sucellus", 42, 1),
+    Metric("General Graardor", 43, 1),
+    Metric("Giant Mole", 44, 1),
+    Metric("Grotesque Guardians", 45, 1),
+    Metric("Hespori", 46, 1),
+    Metric("Kalphite Queen", 47, 1),
+    Metric("King Black Dragon", 48, 1),
+    Metric("Kraken", 49, 1),
+    Metric("Kree'arra", 50, 1),
+    Metric("K'ril Tsutsaroth", 51, 1),
+    Metric("Lunar Chests", 52, 1),
+    Metric("Mimic", 53, 1),
+    Metric("Nex", 54, 1),
+    Metric("Nightmare", 55, 1),
+    Metric("Phosani's Nightmare", 56, 1),
+    Metric("Obor", 57, 1),
+    Metric("Phantom Muspah", 58, 1),
+    Metric("Sarachnis", 59, 1),
+    Metric("Scorpia", 60, 1),
+    Metric("Scurrius", 61,),
+    Metric("Skotizo", 62, 1),
+    Metric("Sol Heredit", 63, 1),
+    Metric("Spindel", 64, 1),
+    Metric("Tempoross", 65, 1),
+    Metric("The Gauntlet", 66, 1),
+    Metric("The Corrupted Gauntlet", 67, 1),
+    Metric("The Hueycoatl", 68, 1),
+    Metric("The Leviathan", 69, 1),
+    Metric("The Royal Titans", 70, 1),
+    Metric("The Whisperer", 71, 1),
+    Metric("Theatre of Blood", 72, 1),
+    Metric("Theatre of Blood: Hard Mode", 73, 1),
+    Metric("Themonuclear Smoke Devil", 74, 1),
+    Metric("Tombs of Amascut", 75, 1),
+    Metric("Tombs of Amascut: Expert Mode", 76, 1),
+    Metric("TzKal-Zuk", 77, 1),
+    Metric("TzTok-Jad", 78, 1),
+    Metric("Vardorvis", 79, 1),
+    Metric("Venenatis", 80, 1),
+    Metric("Vet'ion", 81, 1),
+    Metric("Vorkath", 82, 1),
+    Metric("Wintertodt", 83, 1),
+    Metric("Yama", 84, 1),
+    Metric("Zalcano", 85, 1),
+    Metric("Zulrah", 86, 1),
 ]
 
 if ENABLE_SEASONAL:
     # Add league points
-    METRICS.insert(0, Metric("League Points", 0, 1))
+    METRICS.insert(0, Metric("League Points", 1, 1))
 else:
     # Add bounty hunter
     METRICS.extend(
         {
-            Metric("Bounty Hunter Hunter", 2, 1),
-            Metric("Bounty Hunter Rogue", 3, 1),
-            Metric("Bounty Hunter Hunter (Legacy)", 4, 1),
-            Metric("Bounty Hunter Rogue (Legacy)", 5, 1),
+            Metric("Bounty Hunter Hunter", 3, 1),
+            Metric("Bounty Hunter Rogue", 4, 1),
+            Metric("Bounty Hunter Hunter (Legacy)", 5, 1),
+            Metric("Bounty Hunter Rogue (Legacy)", 6, 1),
         }
     )
 
@@ -409,93 +378,45 @@ else:
 # START Utilities
 #########################################################
 
-
-def clean_table_data(data: str) -> str:
-    """Strips and removes commas from the strings to integers can be parsed."""
-    return data.strip().replace(",", "")
-
-
-def clean_username(username: str) -> str:
-    """Strips and replaces &nbsp; with a space."""
-    return username.strip().replace(u"\xa0", " ")
-
-
-def parse_leader(metric: Metric, row: t.Tuple[str, ...]) -> MetricLeader:
-    """Parses a metric leader out of a tuple of strings holding the necessary
-    data points.
-    """
-    if metric.category:
-        # This is an activity or boss
-        rank, username, score = row
-        return NonSkillLeader(
-            metric,
-            clean_username(username),
-            int(clean_table_data(rank)),
-            int(clean_table_data(score)),
-        )
-    else:
-        # This is a skill
-        rank, username, level, exp = row
-        return SkillLeader(
-            metric,
-            clean_username(username),
-            int(clean_table_data(rank)),
-            int(clean_table_data(level)),
-            int(clean_table_data(exp)),
-        )
-
-
-def parse_leaders(metric: Metric, data: Tag) -> t.List[MetricLeader]:
-    """Transforms the parsed html table data into a list of metric leaders."""
-    # Skills have 4 columns, bosses and activities have 3
-    columns = 3 if metric.category else 4
-
-    # Remove empty columns (bad data at the beginning of each table)
-    # and group the necessary number of columns based on metric
-    rows = zip(*[iter(row.text for row in data if row.text)] * columns)
-
-    # Parse and return the metric leaders
-    return [parse_leader(metric, row) for row in rows]
-
-
-def build_url(metric: Metric, page: int) -> str:
+def build_url(metric: Metric, rank: int) -> str:
     """Builds the URL to use to fetch leaders for the given metric."""
-    params = {"category_type": metric.category, "table": metric.table, "page": page}
+    params = {"category": metric.category, "table": metric.table, "size": PLAYERS_PER_PAGE, "toprank": rank}
     query = "&".join(f"{k}={v}" for k, v in params.items() if v is not None)
     mode = "hiscore_oldschool"
 
     if ENABLE_SEASONAL:
         mode += "_seasonal"
 
-    return f"{BASE_URL}/m={mode}/overall?{query}"
-
+    return f"{BASE_URL}/m={mode}/ranking.json?{query}"
 
 #########################################################
 # END Utilities
 #########################################################
 
+async def fetch_hiscore_players(session: ClientSession, metric: Metric, rank: int = 0) -> t.List[HiscorePlayer]:
+    url = build_url(metric, rank)
 
-async def fetch_leaders(session: ClientSession, metric: Metric, page: int = 1) -> t.List[MetricLeader]:
-    url = build_url(metric, page)
-
-    # Fetch the hiscores data for this metric
     response = await session.get(url)
-    text = await response.text()
+    if response.status != 200:
+        return
+    
+    json_data = await response.json()
+    hiscores_players = [
+        HiscorePlayer(
+            name=player_data["name"],
+            score=int(player_data["score"].replace(',', '')),
+            rank=int(player_data["rank"].replace(',', ''))
+        )
+        for player_data in json_data
+    ] 
 
-    # Parse the HTML down to the table data
-    soup = BeautifulSoup(text, "html.parser")
-    table = t.cast(Tag, soup.findChild("table"))
-    rows = t.cast(Tag, table.findChildren("td"))
+    return hiscores_players
 
-    # Parse and return the metric leaders
-    return parse_leaders(metric, rows)
-
-
-async def fetch_all_leaders(session: ClientSession) -> t.List[MetricLeader]:
+async def fetch_all_leaders(session: ClientSession) -> t.List[HiscorePlayer]:
     metric_limit = METRIC_LIMIT if METRIC_LIMIT else len(METRICS)
-    metric_leaders: t.List[MetricLeader] = []
-    is_unique: t.Callable[[MetricLeader], bool] = lambda l: not any(
-        l.username == m.username for m in metric_leaders
+    metric_leaders: t.List[HiscorePlayer] = []
+    is_unique: t.Callable[[HiscorePlayer], bool] = lambda l: not any(
+        l.name == m.name for m in metric_leaders
     )
 
     for i in range(metric_limit):
@@ -504,12 +425,11 @@ async def fetch_all_leaders(session: ClientSession) -> t.List[MetricLeader]:
 
         try:
             # Fetch leaders in this metric
-            leaders = await fetch_leaders(session, metric)
+            leaders = await fetch_hiscore_players(session, metric)
             LOGGER.info(f"Found {len(leaders)} leaders for {metric}")
 
             if LOG_LEVEL <= logging.DEBUG:
-                for leader in leaders:
-                    LOGGER.debug(leader)
+                LOGGER.debug([leader.name for leader in leaders])
 
             # Filter out players we have already seen
             leaders = list(filter(is_unique, leaders))
@@ -528,7 +448,7 @@ async def fetch_all_leaders(session: ClientSession) -> t.List[MetricLeader]:
     return metric_leaders
 
 
-async def submit_updates(leaders: t.List[MetricLeader]) -> None:
+async def submit_updates(leaders: t.List[HiscorePlayer]) -> None:
     client = wom.Client(user_agent=WOM_USER_AGENT)
     await client.start()
 
